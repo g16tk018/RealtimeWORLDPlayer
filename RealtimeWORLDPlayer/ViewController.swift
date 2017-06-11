135dae5015eb06c61ab5650236848f310b337b69
g16tk018 <g16tk018@yamanashi.ac.jp>
2017-06-11 14:40:48 +0900

    Initial Commit

---
+++ RealtimeWORLDPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealtimeWORLDPlayer
+//
+//  Created by Yusuke Watanabe on 2017/06/11.
+//  Copyright © 2017年 Yusuke Watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
